8733e156adbeb06ec8d4fe28da8b6a328496a849
Foone Turing <fooneturing@gmail.com>
2015-01-24 22:59:08 -0800

    Switching to random-based chunk IDs

--- code/idgenerator.py
+++ code/idgenerator.py
@@ -1,6 +1,6 @@
 #!/usr/env python
 #md2_to_vxp: Converts Quake 2 MD2 files to v3dmm expansions (VXPs)
-#Copyright (C) 2004 Travis Wells / Philip D. Bober
+#Copyright (C) 2004-2014 Foone Turing
 #
 #This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 #
@@ -8,15 +8,9 @@
 #
 #You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-from time import time
+import random
 def GenerateID():
-	start_time=1098499084
-	end_time=1414031949
-	diff=float(end_time-start_time)
-	a=(time()-start_time)/diff
 	min=1000000
 	max=4294967295
-	diff=max-min
-	b=int(min+a*diff)
-	return b
+	return random.randint(min,max)
 
